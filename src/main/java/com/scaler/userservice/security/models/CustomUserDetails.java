b8e8905276feea1f192c65874bddd9d49269141b
vmadhugithub <go2v.madhu@gmail.com>
2024-11-03 21:08:34 +0530

    security related changes

---
+++ src/main/java/com/scaler/userservice/security/models/CustomUserDetails.java
@@ -0,0 +1,98 @@
+package com.scaler.userservice.security.models;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.scaler.userservicemwfeve.models.Role;
+import com.scaler.userservicemwfeve.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@JsonDeserialize
+public class CustomUserDetails implements UserDetails {
+//    private User user;
+    private List<CustomGrantedAuthority> authorities;
+    private String password;
+    private String username;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+    private Long userId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public CustomUserDetails() {}
+
+    public CustomUserDetails(User user) {
+//        this.user = user;
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.enabled = true;
+        this.credentialsNonExpired = true;
+        this.password = user.getHashedPassword();
+        this.username = user.getEmail();
+        this.userId = user.getId();
+
+        List<CustomGrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        for (Role role: user.getRoles()) {
+            grantedAuthorities.add(new CustomGrantedAuthority(role));
+        }
+
+        this.authorities = grantedAuthorities;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        List<CustomGrantedAuthority> grantedAuthorities = new ArrayList<>();
+//
+//        for (Role role: user.getRoles()) {
+//            grantedAuthorities.add(new CustomGrantedAuthority(role));
+//        }
+//
+//        return grantedAuthorities;
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+//        return user.getHashedPassword();
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+//        return user.getEmail();
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
